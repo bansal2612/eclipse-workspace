e8da2f62f16cdbc9d8e3ce3eae1aaf412596cf9f
bansal2612 <58508850+bansal2612@users.noreply.github.com>
2020-06-22 07:17:32 +0530

    Utility Change

--- testNG/src/test/java/com/testNG/Utility.java
+++ testNG/src/test/java/com/testNG/Utility.java
@@ -13,7 +13,7 @@ public class Utility {
 	public static String takeScreenShot(WebDriver driver, String ScreenshotName)
 	{
 		String path =System.getProperty("user.dir") + "\\Reports\\ScreenShots\\" + ScreenshotName + System.currentTimeMillis() + ".png";
-		try {
+		try  {
 			TakesScreenshot ts=(TakesScreenshot)driver;
 			File f=ts.getScreenshotAs(OutputType.FILE);			
 			File dest=new File(path);
